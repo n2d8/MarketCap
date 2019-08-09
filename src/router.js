b7509b4d2537a13a0a311283a07383b302f25222
n2d8 <09orrp1@gmail.com>
2019-08-09 15:58:11 +0800

    add switch to router

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 
 import App from './components/App';
 import NotFound from './components/NotFound';
@@ -7,10 +7,12 @@ import NotFound from './components/NotFound';
 const AppRouter = () => {
   return (
     <Router>
-      <Route
-        exact path="/"
-        component={App} />
-      <Route component={NotFound} />
+      <Switch>
+        <Route
+          exact path="/"
+          component={App} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 };
