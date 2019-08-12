10481be655a4b61793c6c2e830852d79a7aa6103
n2d8 <09orrp1@gmail.com>
2019-08-12 11:03:35 +0800

    move mui theme provider

--- src/components/App.js
+++ src/components/App.js
@@ -2,41 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../styles/App.css';
 import * as MarketCapActions from '../actions/marketCap.actions';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 class App extends Component {
   constructor(props) {
     super(props);
     if(! this.props.listings) {
       this.props.dispatch(MarketCapActions.getCoinListing());
-      // const options = {
-      //   headers: {
-      //     'X-CMC_PRO_API_KEY': Key.COIN_MARKET_CAP_API_KEY
-      //   }
-      // };
-      // fetch('/listings/latest?start=1&limit=5000&convert=USD', options).then(res => {
-      //   console.log('API res', res);
-      // }).catch(err => {
-      //   console.log('API err', err);
-      // });
     }
   }
   render() {
     return(
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+      </MuiThemeProvider>
     );
   }
 }
