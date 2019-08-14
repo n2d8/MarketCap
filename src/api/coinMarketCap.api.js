cd2393e5a496dfe5a9bd6630321c86f13b79367c
n2d8 <09orrp1@gmail.com>
2019-08-15 04:04:18 +0800

    change styles and header

--- src/api/coinMarketCap.api.js
+++ src/api/coinMarketCap.api.js
@@ -1,11 +1,25 @@
 import * as Key from './apiKey';
 
+const options = {
+  headers: {
+    'X-CMC_PRO_API_KEY': Key.COIN_MARKET_CAP_API_KEY
+  }
+};
+
+export function getCryptocurrencyInfo(id) {
+  return new Promise((resolve, reject) => {
+    fetch('/listings/latest?start=1&limit=5000&convert=USD', options).then(res => {
+      if(!res.ok) {
+        reject(res);
+      }
+      resolve(res.json());
+    }).catch(err => {
+      reject(err.message);
+    });
+  });
+}
+
 export function getMarketCapListing() {
-  const options = {
-    headers: {
-      'X-CMC_PRO_API_KEY': Key.COIN_MARKET_CAP_API_KEY
-    }
-  };
   return new Promise((resolve, reject) => {
     const res = {
       "status": {
