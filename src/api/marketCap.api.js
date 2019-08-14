88e6b7b2fa4528d6d24553f168e7c045b2dd6e45
n2d8 <09orrp1@gmail.com>
2019-08-14 18:43:45 +0800

    change token location for confirm call works

--- src/api/marketCap.api.js
+++ src/api/marketCap.api.js
@@ -1,11 +1,11 @@
 export function getMarketCapListing() {
   const options = {
-    headers: {
-      'X-CMC_PRO_API_KEY': '8ea7e2f7-dae8-4834-9162-6a0787662aa6'
-    }
+    // headers: {
+    //   'X-CMC_PRO_API_KEY': '8ea7e2f7-dae8-4834-9162-6a0787662aa6'
+    // }
   };
   return new Promise((resolve, reject) => {
-    fetch('/api/listings/latest?start=1&limit=5000&convert=USD', options).then(res => {
+    fetch('/api/listings/latest?start=1&limit=5000&convert=USD&CMC_PRO_API_KEY=8ea7e2f7-dae8-4834-9162-6a0787662aa6', options).then(res => {
       console.log('the provided api key', process.env.COIN_MARKET_CAP_API_KEY);
       if(!res.ok) {
         console.log('API call respond but not ok', res);
