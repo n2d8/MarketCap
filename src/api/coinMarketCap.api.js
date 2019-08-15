c5e9c25c161c02f0f7d489fb614183b87ef96ced
n2d8 <09orrp1@gmail.com>
2019-08-15 08:07:40 +0800

    change endpoint to fit proxy

--- src/api/coinMarketCap.api.js
+++ src/api/coinMarketCap.api.js
@@ -1,6 +1,6 @@
 export function getCryptocurrencyInfo(id) {
   return new Promise((resolve, reject) => {
-    fetch('/info?id=' + id + '&CMC_PRO_API_KEY=dd3640d7-16f5-4d04-a0e9-0c27391c7a33').then(res => {
+    fetch('/api/info?id=' + id + '&CMC_PRO_API_KEY=dd3640d7-16f5-4d04-a0e9-0c27391c7a33').then(res => {
       if(!res.ok) {
         reject(res);
       }
@@ -13,7 +13,7 @@ export function getCryptocurrencyInfo(id) {
 
 export function getMarketCapListing() {
   return new Promise((resolve, reject) => {
-    fetch('/listings/latest?start=1&limit=5000&convert=USD&CMC_PRO_API_KEY=dd3640d7-16f5-4d04-a0e9-0c27391c7a33').then(res => {
+    fetch('/api/listings/latest?start=1&limit=5000&convert=USD&CMC_PRO_API_KEY=dd3640d7-16f5-4d04-a0e9-0c27391c7a33').then(res => {
       if(!res.ok) {
         reject(res);
       }
